84ef29f50d1e80d06c715b6f7b2857e2bcb556ec
Jon Agoo <jonagooo01@gmail.com>
2024-01-30 20:11:34 +0800

    Section 27 - Drivers App How to Receive Push Notifications using FCM tokens

--- lib/pushNotification/push_notification_system.dart
+++ lib/pushNotification/push_notification_system.dart
@@ -20,5 +20,39 @@ class PushNotificationSystem
 
     firebaseCloudMessaging.subscribeToTopic("drivers");
     firebaseCloudMessaging.subscribeToTopic("users");
+
   }
+
+  // listening for new notifications (covers 3 scenarios)
+  startListeningForNewNotification()
+  {
+    // 1. App is terminated (app completely closed)
+    FirebaseMessaging.instance.getInitialMessage().then((RemoteMessage? messageRemote)
+    {
+     if(messageRemote != null)
+       {
+         String tripID =  messageRemote.data["tripID"];
+       }
+    });
+
+    // 2. Fpreground (app is open)
+    FirebaseMessaging.onMessage.listen((RemoteMessage? messageRemote)
+    {
+      if(messageRemote != null)
+      {
+        String tripID =  messageRemote.data["tripID"];
+      }
+    });
+
+    // 3. Background (app is in background)
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage? messageRemote)
+    {
+      if(messageRemote != null)
+      {
+        String tripID =  messageRemote.data["tripID"];
+      }
+    });
+    
+  }
+
 }
